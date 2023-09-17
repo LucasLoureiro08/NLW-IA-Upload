e979a621d7917310e116f92d292c98d422d39ef1
Lucas Loureiro <lucasloureiro0805@gmail.com>
2023-09-17 20:16:25 -0300

    the dependencies of the ffmpeg were uploaded and fixed

--- src/lib/ffmpeg.ts
+++ src/lib/ffmpeg.ts
@@ -1,10 +1,9 @@
 import { FFmpeg } from '@ffmpeg/ffmpeg';
-import {toBlobURL} from '@ffmpeg/util'
 
-const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.3/dist/umd'
-// import coreURL from '../ffmpeg/ffmpeg-core.js?url';
-// import wasmURL from '../ffmpeg/ffmpeg-core.wasm?url';
-// import workerURL from '../ffmpeg/ffmpeg-worker.js?url';
+// const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.3/dist/umd'
+import coreURL from '../ffmpeg/ffmpeg-core.js?url';
+import wasmURL from '../ffmpeg/ffmpeg-core.wasm?url';
+import workerURL from '../ffmpeg/ffmpeg-worker.js?url';
 
 let ffmpeg: FFmpeg | null;
 
@@ -12,16 +11,15 @@ async function getFFmpeg() {
   if (ffmpeg) {
     return ffmpeg
   }
-
   ffmpeg = new FFmpeg();
 
   if (!ffmpeg.loaded) {
     await ffmpeg.load({
-      // coreURL,
-      // wasmURL,
-      // workerURL,
-      coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
-      wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
+      coreURL,
+      wasmURL,
+      workerURL,
+      // coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
+      // wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
 
     });
   }
